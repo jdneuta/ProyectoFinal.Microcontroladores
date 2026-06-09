6f70fcad3e1e4a77f0097f445f56f9ef31288863
mariadcastillo <mariadcastillo@unicauca.edu.co>
2026-06-09 08:29:48 -0500

    Update i2c.c

--- SOURCE FILES/i2c.c
+++ SOURCE FILES/i2c.c
@@ -1,21 +1,17 @@
-
 #include "i2c.h"
 #include "config.h"
 
 #ifdef I2C_MASTER_MODE
-
 void I2C_Init_Master(unsigned char sp_i2c)
 {
     TRIS_SCL = 1;                   //SCL como entrada
     TRIS_SDA = 1;                   //SDA como entrada
+    SSPSTAT = sp_i2c;               //Variable para frecuencia    
 
-    SSPSTAT = sp_i2c;               //Slew rate y velocidad    
-
-    SSPCON1 = 0x28;                 // Habilita el modulo MSSP en modo I2C maestro
-    SSPCON2 = 0x00;                 // Limpia registros de control adicionales del MSSP
+    SSPCON1 = 0x28;                 //Habilita el modulo MSSP en modo I2C maestro
+    SSPCON2 = 0x00;                 //Limpia registros de control adicionales del MSSP
     PIR1bits.SSPIF = 0;             // Limpia la bandera de interrupcion SSP
 
-    /** Define el divisor de baud rate segun la velocidad solicitada. */
     if(sp_i2c == I2C_100KHZ)
     {
         SSPADD = 19;
@@ -28,68 +24,66 @@ void I2C_Init_Master(unsigned char sp_i2c)
 
 void I2C_Start(void)
 {
-    PIR1bits.SSPIF = 0;
-    SSPCON2bits.SEN = 1;
-    while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
-}
-
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
+    SSPCON2bits.SEN = 1;            //Genera condicion de start
+    while(PIR1bits.SSPIF == 0);     //Espera a que termine start
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
+}               
 
 void I2C_Stop(void)
 {
-    PIR1bits.SSPIF = 0;
-    SSPCON2bits.PEN = 1;
-    while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
+    SSPCON2bits.PEN = 1;            //Genera condicion de stop
+    while(PIR1bits.SSPIF == 0);     //Espera a que termine stop
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
 }
 
 void I2C_Restart(void)
 {
-    PIR1bits.SSPIF = 0;
-    SSPCON2bits.RSEN = 1;
-    while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
+    SSPCON2bits.RSEN = 1;           //Genero condicion de reseteo 
+    while(PIR1bits.SSPIF == 0);     //Espera a que termine reseteo
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
 }
 
 void I2C_Ack(void)
 {
-    PIR1bits.SSPIF = 0;
-    SSPCON2bits.ACKDT = 0;
-    SSPCON2bits.ACKEN = 1;
-    while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
+    SSPCON2bits.ACKDT = 0;          //Elige ACK porque es 0
+    SSPCON2bits.ACKEN = 1;          //Inicializa la secuencia en SDA /SCL 
+    while(PIR1bits.SSPIF == 0);     
+    PIR1bits.SSPIF = 0;             //Limpia la bandera de interrupción del módulo MSSP
 }
 
 void I2C_Nack(void)
 {
-    PIR1bits.SSPIF = 0;
-    SSPCON2bits.ACKDT = 1;
-    SSPCON2bits.ACKEN = 1;
+    PIR1bits.SSPIF = 0;              //Limpia la bandera de interrupción del módulo MSSP
+    SSPCON2bits.ACKDT = 1;           //Habilita NACK porque es 1 
+    SSPCON2bits.ACKEN = 1;           //Inicializa la secuencia en SDA /SCL   
     while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
+    PIR1bits.SSPIF = 0;              //Limpia la bandera de interrupción del módulo MSSP
 }
 
 short I2C_Write(char data)
 {
-    PIR1bits.SSPIF = 0;
-    SSPBUF = data;
+    PIR1bits.SSPIF = 0;              //Limpia la bandera de interrupción del módulo MSSP
+    SSPBUF = data;                   //Almacena datos mientras se envia 
     while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
-    return SSPCON2bits.ACKSTAT;
+    PIR1bits.SSPIF = 0;              //Limpia la bandera de interrupción del módulo MSSP
+    return SSPCON2bits.ACKSTAT;      //Indica si recibe o no  algo del esclavo
 }
 
 unsigned char I2C_Read(void)
 {
-    PIR1bits.SSPIF = 0;
-    SSPCON2bits.RCEN = 1;
-    while(PIR1bits.SSPIF == 0);
-    PIR1bits.SSPIF = 0;
-    return SSPBUF;
+    PIR1bits.SSPIF = 0;              //Limpia la bandera de interrupción del módulo MSSP
+    SSPCON2bits.RCEN = 1;            //Habilita la recepcion
+    while(PIR1bits.SSPIF == 0);      //Espera que termine de recibir
+    PIR1bits.SSPIF = 0;              //Limpia la bandera de interrupción del módulo MSSP
+    return SSPBUF;                   //Retorna lo que recibe 
 }
 #endif
 
 #ifdef I2C_SLAVE_MODE
-
 void I2C_Init_Slave(unsigned char add_slave)
 {
     TRIS_SCL = 1;
@@ -104,13 +98,11 @@ void I2C_Init_Slave(unsigned char add_slave)
     PIE1bits.SSPIE = 1;
 }
 
-
 short I2C_Error_Read(void)
 {
     SSPCON1bits.CKP = 0;
     return ((SSPCON1bits.SSPOV) || (SSPCON1bits.WCOL)) ? 1 : 0;
 }
-
 
 short I2C_Write_Mode(void)
 {
@@ -131,7 +123,6 @@ void I2C_Error_Data(void)
     SSPCON1bits.WCOL = 0;
     SSPCON1bits.CKP = 1;
     SSPCON1bits.SSPM3 = 0;
-    (void)z;
 }
 
 unsigned char I2C_Read_Slave(void)
@@ -146,10 +137,8 @@ unsigned char I2C_Read_Slave(void)
     dato_i2c = SSPBUF;
     SSPCON1bits.CKP = 1;
     SSPCON1bits.SSPM3 = 0;
-    (void)z;
     return dato_i2c;
 }
-
 
 void I2C_Write_Slave(char dato_i2c)
 {
@@ -162,6 +151,6 @@ void I2C_Write_Slave(char dato_i2c)
     {
         ;
     }
-    (void)z;
 }
 #endif
+
