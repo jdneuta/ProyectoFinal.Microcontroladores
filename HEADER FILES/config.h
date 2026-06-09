c17dd17b93eefd3d6503c552c81a4cba06e03f28
mariadcastillo <mariadcastillo@unicauca.edu.co>
2026-06-09 08:12:20 -0500

    Update config.h

--- HEADER FILES/config.h
+++ HEADER FILES/config.h
@@ -1,49 +1,77 @@
 /* 
  * File:   config.h
- * Author: MARIA DE LOS ANGELES
+ * Author: María De Los Ángeles Castillo 
  *
- * Created on 21 de mayo de 2026, 17:02
+ * Created on 18 de abril de 2026, 01:51 PM
  */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// ============================================================
+// CONFIG BITS -- Oscilador interno 8MHz
+// ============================================================
+
+// CONFIG1L
+#pragma config PLLDIV   = 1         // Sin división (PLL no se usa)
+#pragma config CPUDIV   = OSC1_PLL2 // CPU corre a Fosc/1
+#pragma config USBDIV   = 1         // USB no se usa
+
+// CONFIG1H
+#pragma config FOSC     = INTOSC_EC // Oscilador interno, sin cristal 
+#pragma config FCMEN    = OFF
+#pragma config IESO     = OFF
+
+// CONFIG2L
+#pragma config PWRT     = ON        // Power-up timer ON
+#pragma config BOR      = ON        // Brown-out reset ON
+#pragma config BORV     = 3
+#pragma config VREGEN   = OFF
+
+// CONFIG2H
+#pragma config WDT      = OFF
+#pragma config WDTPS    = 32768
+
+// CONFIG3H
+#pragma config CCP2MX   = ON        // CCP2 en RC1
+#pragma config PBADEN   = OFF       // PORTB digital al reset
+#pragma config LPT1OSC  = OFF
+#pragma config MCLRE    = OFF
+
+// CONFIG4L
+#pragma config STVREN   = ON
+#pragma config LVP      = OFF
+#pragma config ICPRT    = OFF
+#pragma config XINST    = OFF
+
+// CONFIG5L/5H/6L/6H/7L/7H
+#pragma config CP0=OFF, CP1=OFF, CP2=OFF, CP3=OFF
+#pragma config CPB=OFF, CPD=OFF
+#pragma config WRT0=OFF, WRT1=OFF, WRT2=OFF, WRT3=OFF
+#pragma config WRTB=OFF, WRTC=OFF, WRTD=OFF
+#pragma config EBTR0=OFF, EBTR1=OFF, EBTR2=OFF, EBTR3=OFF
+#pragma config EBTRB=OFF
 
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include <stdint.h>
-#include <stdbool.h>
 #include <xc.h>
 
 #define _XTAL_FREQ 8000000UL
-#pragma config PLLDIV   = 1         /**< Sin division para PLL, ya que no se usa. */
-#pragma config CPUDIV   = OSC1_PLL2 /**< CPU a Fosc/1 en esta configuracion. */
-#pragma config USBDIV   = 1         /**< USB no utilizado. */
 
-#pragma config FOSC     = INTOSC_EC /**< Oscilador interno, sin cristal externo. */
-#pragma config FCMEN    = OFF       /**< Monitor de falla de reloj deshabilitado. */
-#pragma config IESO     = OFF       /**< Cambio interno/externo de oscilador deshabilitado. */
+/*static inline void OSC_Init(void)
+{
+    OSCCONbits.IRCF2 = 1;
+    OSCCONbits.IRCF1 = 1;
+    OSCCONbits.IRCF0 = 1;
 
-#pragma config PWRT     = ON        /**< Power-up Timer habilitado. */
-#pragma config BOR      = ON        /**< Brown-out Reset habilitado. */
-#pragma config BORV     = 3         /**< Nivel de voltaje BOR configurado en opcion 3. */
-#pragma config VREGEN   = OFF       /**< Regulador USB deshabilitado. */
+    OSCCONbits.SCS1 = 1;
+    OSCCONbits.SCS0 = 0;
 
-#pragma config WDT      = OFF       /**< Watchdog Timer deshabilitado. */
-#pragma config WDTPS    = 32768     /**< Postscaler del WDT, sin efecto si WDT esta apagado. */
-
-#pragma config CCP2MX   = ON        /**< CCP2 asignado a RC1. */
-#pragma config PBADEN   = OFF       /**< PORTB inicia como digital al reset. */
-#pragma config LPT1OSC  = OFF       /**< Oscilador de baja potencia de Timer1 deshabilitado. */
-#pragma config MCLRE    = OFF       /**< Pin MCLR usado como entrada digital. */
-
-#pragma config STVREN   = ON        /**< Reset por desbordamiento/subdesbordamiento de pila habilitado. */
-#pragma config LVP      = OFF       /**< Programacion de bajo voltaje deshabilitada. */
-#pragma config ICPRT    = OFF       /**< Puerto de depuracion/programacion deshabilitado. */
-#pragma config XINST    = OFF       /**< Set extendido de instrucciones deshabilitado. */
-
-#pragma config CP0=OFF, CP1=OFF, CP2=OFF, CP3=OFF   /**< Proteccion de codigo deshabilitada. */
-#pragma config CPB=OFF, CPD=OFF                     /**< Proteccion de boot block y EEPROM deshabilitada. */
-#pragma config WRT0=OFF, WRT1=OFF, WRT2=OFF, WRT3=OFF /**< Proteccion contra escritura deshabilitada. */
-#pragma config WRTB=OFF, WRTC=OFF, WRTD=OFF         /**< Proteccion de escritura adicional deshabilitada. */
-#pragma config EBTR0=OFF, EBTR1=OFF, EBTR2=OFF, EBTR3=OFF /**< Proteccion de lectura de tabla deshabilitada. */
-#pragma config EBTRB=OFF                            /**< Proteccion de lectura de boot block deshabilitada. */
+    while(!OSCCONbits.IOFS)
+    {
+        ;
+    }
+}*/
 
 #endif
